8848e7d3020478f40984f805bd820d6977d155d8
Conner McKenna <cmckenna@ellisdon.com>
2021-03-22 19:46:42 -0400

    ddd

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -2,9 +2,13 @@
 echo "\n--------------------------------"
 echo "------> Generating .profile.d file to generate google-credentials.json at startup"
 
-if [ -f .env ]
+if [ -f .env  ]
 then
   export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+if [ -f .config  ]
+then
+  export $(cat .config | sed 's/#.*//g' | xargs)
 fi
 
 DECODED=$(echo $GOOGLE_CREDENTIALS | base64 --d)
